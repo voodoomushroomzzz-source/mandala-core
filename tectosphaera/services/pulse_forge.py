c25b9c413c870e4e4bce0cafc7b82e4f5418c958
voodoomushroomzzz-source <voodoomushroomzzz@gmail.com>
2026-02-21 22:08:19 +0300

    📝 Патч от mc_mlvan | 19:08

--- tectosphaera/services/pulse_forge.py
+++ tectosphaera/services/pulse_forge.py
@@ -1,8 +1,11 @@
+import asyncio
 from datetime import datetime
+
 from fastapi import WebSocket
 
 PULSE_INTERVAL = 30
 ws_clients: list[WebSocket] = []
+
 
 async def beat():
     while True:
@@ -17,9 +20,23 @@ async def beat():
             ws_clients.remove(ws)
         await asyncio.sleep(PULSE_INTERVAL)
 
+
 async def register(ws: WebSocket):
     ws_clients.append(ws)
+
 
 async def unregister(ws: WebSocket):
     if ws in ws_clients:
         ws_clients.remove(ws)
+
+
+async def ws_handler(websocket: WebSocket):
+    await websocket.accept()
+    await register(websocket)
+    try:
+        while True:
+            _ = await websocket.receive_text()
+    except Exception:
+        pass
+    finally:
+        await unregister(websocket)
