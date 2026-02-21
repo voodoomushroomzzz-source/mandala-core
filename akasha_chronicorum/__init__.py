270a9ebfb66789d40b8c327fa91d44b51f2bc12c
voodoomushroomzzz-source <voodoomushroomzzz@gmail.com>
2026-02-22 00:17:39 +0300

    Delete akasha_chronicorum directory

--- akasha_chronicorum/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Akasha Chronicorum – хранилище событий и артефактов Мандалы."""
-from .core import push_event, get_events, get_artifacts  # экспортируем нужное
-__all__ = ["push_event", "get_events", "get_artifacts"]
