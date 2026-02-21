869fde2c915d88d8b2570d4050a3dcea6444ab2e
voodoomushroomzzz-source <voodoomushroomzzz@gmail.com>
2026-02-21 22:08:18 +0300

    📝 Патч от mc_mlvan | 19:08

--- tectosphaera/routes/pulse.py
+++ tectosphaera/routes/pulse.py
@@ -1,13 +1,11 @@
 """Роуты для pulse-forge."""
-from fastapi import APIRouter, WebSocket, WebSocketDisconnect
-from tectosphaera.services.pulse_forge import ws_handler, start_pulse_task
+from fastapi import APIRouter, WebSocket
+
+from tectosphaera.services.pulse_forge import start_pulse_task, ws_handler
 
 router = APIRouter(prefix="/pulse")
 
-@router.on_event("startup")
-async def startup_pulse():
-    start_pulse_task()
 
 @router.websocket("/ws")
-async def websocket_pulse(websocket: WebSocket):
+async def websocket_endpoint(websocket: WebSocket):
     await ws_handler(websocket)
