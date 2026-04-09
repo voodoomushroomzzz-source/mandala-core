38b3eac6e4f069c67dfeeceb5b143d30bc1150b2
voodoomushroomzzz <voodoomushroomzzz@gmail.com>
2026-04-09 22:46:12 +0300

    app: fixed protocol bar buttons (text instead of symbols)

--- app/components/protocol-bar.js
+++ app/components/protocol-bar.js
@@ -2,10 +2,10 @@ class ProtocolBar extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <div class="proto-bar">
-                <button data-proto="internal/onboarding" title="Онбординг"></button>
-                <button data-proto="internal/registry_sync" title="Синхронизация"></button>
-                <button data-proto="internal/ideas_roadmaps" title="Идеи и роадмапы"></button>
-                <button data-proto="deep_analysis" title="Глубокий анализ"></button>
+                <button data-proto="internal/onboarding" title="Онбординг">ON</button>
+                <button data-proto="internal/registry_sync" title="Синхронизация">SYNC</button>
+                <button data-proto="internal/ideas_roadmaps" title="Идеи и роадмапы">IDEAS</button>
+                <button data-proto="deep_analysis" title="Глубокий анализ">DEEP</button>
             </div>
         `;
         this.querySelectorAll('button').forEach(btn => {
