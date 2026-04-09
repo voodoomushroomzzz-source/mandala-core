9868051d3bd212dadf442847215d52e2bceefdfa
voodoomushroomzzz <voodoomushroomzzz@gmail.com>
2026-04-10 00:25:27 +0300

    cleanup: removed app/ and frontend task (obsolete)

--- app/components/hive-view.js
+++
@@ -1,4 +0,0 @@
-class HiveView extends HTMLElement {
-    connectedCallback() { this.innerHTML = `<div class="hive-placeholder">Карта сот  в разработке</div>`; }
-}
-customElements.define('hive-view', HiveView);
--- app/js/state.js
+++
@@ -1,9 +0,0 @@
-export const state = {
-    ws: null,
-    messages: [],
-    connected: false
-};
-export function setConnected(val) {
-    state.connected = val;
-    window.dispatchEvent(new CustomEvent('connection-change', { detail: val }));
-}
