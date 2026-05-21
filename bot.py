83bed4b0f57aa7ef827350966aad5baa5ec7beb2
voodoomushroomzzz <voodoomushroomzzz@gmail.com>
2026-05-21 13:07:11 +0300

    hotfix: remove duplicate line in tgroup_newtask

--- bot.py
+++ bot.py
@@ -1930,7 +1930,6 @@ async def cb_tgroup_newtask(callback: CallbackQuery, state: FSMContext):
     try:
         await callback.message.edit_text(
             f"➕ <b>Новая задача в «{group_display}»</b>\n\nВведи название:",
-Введи название:",
             reply_markup=cancel_kb, parse_mode="HTML"
         )
     except Exception:
